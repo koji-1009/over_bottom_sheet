5e0c2d7ffd5384c90d9a83b151b6067861786571
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-11-29 00:06:29 +0900

    Add open&close animation

--- lib/src/over_bottom_sheet_controller.dart
+++ lib/src/over_bottom_sheet_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/foundation.dart';
@@ -5,17 +6,44 @@ import 'package:flutter/foundation.dart';
 class OverBottomSheetController extends ValueNotifier<double> {
   OverBottomSheetController({
     double ratio = 1.0,
+    this.period = const Duration(
+      milliseconds: 8,
+    ),
+    this.step = 0.05,
   }) : super(ratio);
+
+  final Duration period;
+  final double step;
 
   void updateRatio(double ratio) {
     value = min(max(ratio, 0), 1);
   }
 
   void open() {
-    updateRatio(1);
+    final base = value;
+    Timer.periodic(period, (timer) {
+      final calc = base + timer.tick * step;
+      if (calc >= 1) {
+        timer.cancel();
+        updateRatio(1);
+        return;
+      }
+
+      updateRatio(calc);
+    });
   }
 
   void close() {
-    updateRatio(0);
+    final base = value;
+    Timer.periodic(period, (timer) {
+      final calc = base - timer.tick * step;
+      if (calc <= 0) {
+        timer.cancel();
+        updateRatio(0);
+        return;
+      }
+
+      updateRatio(calc);
+    });
   }
 }
