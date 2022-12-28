761239c7ba30c196b9f796ceaa6c4fa3093b13d9
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-12-29 00:55:21 +0900

    0.0.1

--- README.md
+++ README.md
@@ -1,39 +1,64 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+## OverBottomSheet
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
-
-## Features
-
-TODO: List what your package can do. Maybe include images, gifs, or videos.
-
-## Getting started
-
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+Widget that always displays BottomSheet.
 
 ## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
-
 ```dart
-const like = 'sample';
+final controller = OverBottomSheetController();
+
+OverBottomSheet(
+  clipBehavior: Clip.hardEdge,
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(32),
+      topRight: Radius.circular(32),
+    ),
+  ),
+  sizeOption: const OverBottomSheetSizeOption.mix(
+    maxHeight: 0.8,
+    minHeight: 120,
+    maxWidth: 0.8,
+  ),
+  controller: controller,
+  headerBuilder: (context, ratio) => Center(
+  child: Padding(
+    padding: const EdgeInsets.all(8),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ValueListenableBuilder<double>(
+          valueListenable: controller,
+          builder: (context, value, child) => IconButton(
+            onPressed: () {
+              if (value <= 0.5) {
+                controller.open();
+              } else {
+                controller.close();
+              }
+            },
+            icon: value >= 0.5
+              ? const Icon(Icons.expand_more)
+              : const Icon(Icons.expand_less),
+            ),
+          ),
+          Text('ratio: ${ratio.toStringAsFixed(3)}'),
+        ],
+      ),
+    ),
+  ),
+  content: ListView.builder(
+    itemBuilder: (context, index) => ListTile(
+      title: Text('sheet $index'),
+    ),
+  ),
+  child: Container(
+    color: Colors.indigo,
+    child: ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        title: Text('main $index'),
+      ),
+    ),
+  ),
+),
 ```
-
-## Additional information
-
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -50,60 +50,60 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Demo'),
       ),
-      body: OverBottomSheet(
-        clipBehavior: Clip.hardEdge,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(32),
-            topRight: Radius.circular(32),
-          ),
-        ),
-        sizeOption: const OverBottomSheetSizeOption.mix(
-          maxHeight: 0.8,
-          minHeight: 120,
-          maxWidth: 0.8,
-        ),
-        controller: _controller,
-        headerBuilder: (context, ratio) => Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ValueListenableBuilder<double>(
-                  valueListenable: _controller,
-                  builder: (context, value, child) => IconButton(
-                    onPressed: () {
-                      if (value <= 0.5) {
-                        _controller.open();
-                      } else {
-                        _controller.close();
-                      }
-                    },
-                    icon: value >= 0.5
-                        ? const Icon(Icons.expand_more)
-                        : const Icon(Icons.expand_less),
-                  ),
-                ),
-                Text('ratio: ${ratio.toStringAsFixed(3)}'),
-              ],
+body: OverBottomSheet(
+  clipBehavior: Clip.hardEdge,
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(32),
+      topRight: Radius.circular(32),
+    ),
+  ),
+  sizeOption: const OverBottomSheetSizeOption.mix(
+    maxHeight: 0.8,
+    minHeight: 120,
+    maxWidth: 0.8,
+  ),
+  controller: _controller,
+  headerBuilder: (context, ratio) => Center(
+    child: Padding(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ValueListenableBuilder<double>(
+            valueListenable: _controller,
+            builder: (context, value, child) => IconButton(
+              onPressed: () {
+                if (value <= 0.5) {
+                  _controller.open();
+                } else {
+                  _controller.close();
+                }
+              },
+              icon: value >= 0.5
+                  ? const Icon(Icons.expand_more)
+                  : const Icon(Icons.expand_less),
             ),
           ),
-        ),
-        content: ListView.builder(
-          itemBuilder: (context, index) => ListTile(
-            title: Text('sheet $index'),
-          ),
-        ),
-        child: Container(
-          color: Colors.indigo,
-          child: ListView.builder(
-            itemBuilder: (context, index) => ListTile(
-              title: Text('main $index'),
-            ),
-          ),
-        ),
+          Text('ratio: ${ratio.toStringAsFixed(3)}'),
+        ],
       ),
+    ),
+  ),
+  content: ListView.builder(
+    itemBuilder: (context, index) => ListTile(
+      title: Text('sheet $index'),
+    ),
+  ),
+  child: Container(
+    color: Colors.indigo,
+    child: ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        title: Text('main $index'),
+      ),
+    ),
+  ),
+),
     );
   }
 }
