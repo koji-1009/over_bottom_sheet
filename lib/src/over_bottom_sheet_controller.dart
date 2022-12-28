6a184a59f44bbe2a2b2c6846f8b730cce468a106
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-12-29 00:50:58 +0900

    Add dart doc

--- lib/src/over_bottom_sheet_controller.dart
+++ lib/src/over_bottom_sheet_controller.dart
@@ -2,17 +2,19 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/foundation.dart';
+import 'package:over_bottom_sheet/src/over_bottom_sheet.dart';
 
+/// Controller that manages the percentage of [OverBottomSheet] being drawn out.
 class OverBottomSheetController extends ValueNotifier<double> {
   OverBottomSheetController({
     double ratio = 1.0,
-    this.period = const Duration(
+    this.tick = const Duration(
       milliseconds: 8,
     ),
     this.step = 0.05,
   }) : super(ratio);
 
-  final Duration period;
+  final Duration tick;
   final double step;
 
   void updateRatio(double ratio) {
@@ -21,7 +23,7 @@ class OverBottomSheetController extends ValueNotifier<double> {
 
   void open() {
     final base = value;
-    Timer.periodic(period, (timer) {
+    Timer.periodic(tick, (timer) {
       final calc = base + timer.tick * step;
       if (calc >= 1) {
         timer.cancel();
@@ -35,7 +37,7 @@ class OverBottomSheetController extends ValueNotifier<double> {
 
   void close() {
     final base = value;
-    Timer.periodic(period, (timer) {
+    Timer.periodic(tick, (timer) {
       final calc = base - timer.tick * step;
       if (calc <= 0) {
         timer.cancel();
--- lib/src/over_bottom_sheet_option.dart
+++ lib/src/over_bottom_sheet_option.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:over_bottom_sheet/src/over_bottom_sheet.dart';
 
 part 'over_bottom_sheet_option.freezed.dart';
 
+/// Option to specify height and width of [OverBottomSheet].
 @freezed
 class OverBottomSheetSizeOption with _$OverBottomSheetSizeOption {
+  /// Specify the value of height and width.
   const factory OverBottomSheetSizeOption.fix({
     @Default(double.infinity) double maxWidth,
     @Default(0.0) double minWidth,
@@ -12,6 +15,7 @@ class OverBottomSheetSizeOption with _$OverBottomSheetSizeOption {
     required double minHeight,
   }) = _OverBottomSheetSizeOptionFix;
 
+  /// Specify the ratio of height and width.
   const factory OverBottomSheetSizeOption.ratio({
     @Default(double.infinity) double maxWidth,
     @Default(0.0) double minWidth,
@@ -19,6 +23,10 @@ class OverBottomSheetSizeOption with _$OverBottomSheetSizeOption {
     required double minHeight,
   }) = _OverBottomSheetSizeOptionRatio;
 
+  /// Use a mixture of [OverBottomSheetSizeOption.fix] and
+  /// [OverBottomSheetSizeOption.ratio].
+  /// The size is defined as a ratio if it is less than 1.0,
+  /// and as a fix mode if it is greater than 1.0.
   const factory OverBottomSheetSizeOption.mix({
     @Default(double.infinity) double maxWidth,
     @Default(0.0) double minWidth,
